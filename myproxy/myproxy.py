120b88f3c8dc6c47559206530dfcfbf7874ce821
w4lker <anonymousvser@gmail.com>
2015-08-31 17:52:14 +0800

    data test

--- myproxy/myproxy.py
+++ myproxy/myproxy.py
@@ -9,7 +9,7 @@ import os
 from libmproxy import controller, proxy
 from libmproxy.proxy.server import ProxyServer
 import sys
-import libs.db
+from libs.db import database
 
 class StickyMaster(controller.Master):
     def __init__(self, server):
@@ -24,18 +24,34 @@ class StickyMaster(controller.Master):
 
     def handle_request(self, flow):
         hid = (flow.request.host, flow.request.port)
-        print flow.request.method
-        print flow.request.url
-        print flow.request.headers
+        httpversion = "HTTP/" + str(flow.request.httpversion[0]) + '.' + str(flow.request.httpversion[1])
+#        req = flow.request.method + flow.request.headers + flow.request.body
+#        print req
+        req = flow.request.method +' ' + flow.request.url + ' ' + httpversion + '\n'
+        for key,value in flow.request.headers:
+            req += key + ':' +' ' + value + '\n'
+        req += '\n' + flow.request.body
+        db = database()
+        cur = db.connectdb('./db.sqlite3')
+        sqlcmd = "insert into webmanager_inspector (method,host,url,request)values('"+flow.request.method+"','"+flow.request.host+"','"+flow.request.url+"','"+req+"')"
+        db.modify(cur,sqlcmd)
+        db.close()
         if flow.request.headers["cookie"]:
             self.stickyhosts[hid] = flow.request.headers["cookie"]
         elif hid in self.stickyhosts:
             flow.request.headers["cookie"] = self.stickyhosts[hid]
         flow.reply()
-
+        
+        
     def handle_response(self, flow):
         hid = (flow.request.host, flow.request.port)
-        print type(flow.response.status_code)
+        httpversion = "HTTP/" + str(flow.response.httpversion[0]) + '.' + str(flow.response.httpversion[1])
+        rep = httpversion +' ' + str(flow.response.status_code) +' ' + flow.response.msg + '\n'
+        for key,value in flow.response.headers:
+                rep += key + ':' +' ' + value + '\n'
+        rep += '\n' + flow.request.body
+#        print rep
+        
         if flow.response.headers["set-cookie"]:
             self.stickyhosts[hid] = flow.response.headers["set-cookie"]
         flow.reply()
